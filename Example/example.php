c4ed738ae4bac8ebfdfc6e0566cd3e699347c321
Axmdstar <85846658+Axmdstar@users.noreply.github.com>
2024-05-08 20:26:02 +0300

    Example folder

---
+++ Example/example.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <?php include "../Components/HeadContent.php" ?>
+  
+  <body>
+    
+  <?php include '../Components/NavBar.php' ?>
+
+  
+  <main id="main" class="main">
+        <!-- Content Here  -->
+        <p>hello Class c</p>
+  </main>
+      
+    
+    <!-- <h2>hello</h2> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
