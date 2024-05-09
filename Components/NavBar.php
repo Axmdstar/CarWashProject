71f439b5f82f85e951e90e2e292b6c4a312861db
Axmdstar <85846658+Axmdstar@users.noreply.github.com>
2024-05-09 21:29:41 +0300

    Added Dashboard

--- Components/NavBar.php
+++ Components/NavBar.php
@@ -9,6 +9,33 @@
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div>
 
+    <script>
+  var toggleButton = document.querySelector('.toggle-sidebar-btn');
+
+  // Check if the element exists
+  if (toggleButton) {
+      // Add onclick event handler
+      toggleButton.onclick = function(e) {
+          // Toggle the class on body
+          document.body.classList.toggle('toggle-sidebar');
+      };
+  }
+
+  let selectHeader = document.getElementById('header');
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled');
+      } else {
+        selectHeader.classList.remove('header-scrolled');
+      }
+    };
+
+    window.addEventListener('load', headerScrolled);
+    window.addEventListener('scroll', headerScrolled); // Add this line to listen for scroll events
+  }
+</script>
+
 </header><!-- End Header -->
 
 <?php include "../Components/SideBar.php" ?>
--- Dashboard/Dashboard.php
+++ Dashboard/Dashboard.php
@@ -1,11 +1,129 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php include "../Components/HeadContent.php" ?>
+
 <body>
-    
+
+  <?php include '../Components/NavBar.php' ?>
+
+  <main id="main" class="main">
+
+    <!-- SummaryCards -->
+    <div class="card">
+      <div class="card-body">
+        <h1 class="card-title">Recent Activity</h1>
+
+        <div style="display: flex; flex-direction: row; justify-content: space-between; gap: 20px;">
+
+          <div class="JazzeraCards">
+            <h2>000</h2>
+            <h3>Product</h3>
+          </div>
+
+          <div class="JazzeraCards">
+            <h2>000</h2>
+            <h3>Services</h3>
+          </div>
+
+          <div class="JazzeraCards">
+            <h2>$000</h2>
+            <h3>Revenue</h3>
+          </div>
+          
+        </div>
+      </div>
+    </div><!-- End Default Card -->
+
+
+    <!-- Chart  -->
+    <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Column Chart</h5>
+
+              <!-- Column Chart -->
+              <div id="columnChart"></div>
+
+              <script>
+                document.addEventListener("DOMContentLoaded", () => {
+                  new ApexCharts(document.querySelector("#columnChart"), {
+                    series: [{
+                      name: 'Net Profit',
+                      data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+                    }, {
+                      name: 'Revenue',
+                      data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+                    }, {
+                      name: 'Free Cash Flow',
+                      data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+                    }],
+                    chart: {
+                      type: 'bar',
+                      height: 350
+                    },
+                    plotOptions: {
+                      bar: {
+                        horizontal: false,
+                        columnWidth: '55%',
+                        endingShape: 'rounded'
+                      },
+                    },
+                    dataLabels: {
+                      enabled: false
+                    },
+                    stroke: {
+                      show: true,
+                      width: 2,
+                      colors: ['transparent']
+                    },
+                    xaxis: {
+                      categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+                    },
+                    yaxis: {
+                      title: {
+                        text: '$ (thousands)'
+                      }
+                    },
+                    fill: {
+                      opacity: 1
+                    },
+                    tooltip: {
+                      y: {
+                        formatter: function(val) {
+                          return "$ " + val + " thousands"
+                        }
+                      }
+                    }
+                  }).render();
+                });
+              </script>
+              <!-- End Column Chart -->
+
+            </div>
+          </div>
+          <!-- Chart end  -->
+
+
+          <!-- Table  -->
+          <table class="table table-borderless">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Emp Name</th>
+                    <th scope="col">Username</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  
+                  <tr>
+                    <td>Raheem Lehner</td>
+                    <td>Dynamic Division Officer</td>
+                    <td>47</td>
+                  </tr>
+                </tbody>
+              </table>
+              <!-- Table Ends  -->
+
+  </main>
 </body>
+
 </html>
