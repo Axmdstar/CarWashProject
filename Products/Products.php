34d852df61142ae5cbc6e1a1dde1c57c32fc91b7
UsamaAli98 <usamaali019@outlook.com>
2024-05-10 19:19:06 +0100

    product in admin

---
+++ Products/Products.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <?php include "../Components/HeadContent.php" ?>
+  
+  <body>
+    
+  <?php include '../Components/NavBar.php' ?>
+
+  
+  <main id="main" class="main">
+        <!-- Content Here  -->
+        <div class="card">
+            <div class="card-body">
+                <h1 class="card-title">Add Product</h1>
+
+            </div>
+        </div>
+
+  </main>
+      
+    
+  </body>
