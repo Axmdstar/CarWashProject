400d22d04bb5983cba2fedd36b2e1b89aa2a0d4c
ABDULLAH16 <Abdullaahiabdi9960@gmail.com>
2024-05-13 11:12:14 +0300

    heoolw

--- Components/insert.php
+++ Components/insert.php
@@ -1,11 +1,26 @@
 <?php
-    include('connection.php');
-    if(isset($_POST['add'])){
-        $ServiceName=$_POST['ServiceName'];
-        $category=$_POST['category'];
-        $amount=$_POST['amount'];
+    // include('connection.php');
+    // if(isset($_POST['add'])){
+    //     $CarType=$_POST['Cartype'];
+    //     $category=$_POST['category'];
+    //     $amount=$_POST['amount'];
 
-        $query = mysqli_query($connect, "Insert into services (CarName, Amount, ServiceCategoryId) Values('$ServiceName, $category, $amount");
+    //     $query = mysqli_query($connect, "Insert into services (Cartype, Amount, ServiceCategoryId) Values('$CarType, $category, $amount");
+    //     if($query){
+    //         echo"<script>alert(data inserted successfully)</script>";
+
+    //     } else{
+    //         echo"<script>alert(there is an error)</script>";
+
+    //     }
+    // }
+
+    include('connection.php');
+    if(isset($_POST['add1'])){
+        $CatName=$_POST['CatName'];
+        $CommissionRate=$_POST['CommissionRate'];
+
+        $query = mysqli_query($connect, "Insert into servicecategory (CatName, CommissinRate) Values('$CatName, $CommissionRate");
         if($query){
             echo"<script>alert(data inserted successfully)</script>";
 
@@ -14,6 +29,7 @@
 
         }
     }
+
 
 
 
--- Components/insert1.php
+++ Components/insert1.php
@@ -1,18 +1,4 @@
 <?php
-    include('connection.php');
-    if(isset($_POST['add1'])){
-        $CatName=$_POST['CatName'];
-        $CommissionRate=$_POST['CommissionRate'];
-
-        $query = mysqli_query($connect, "Insert into CatName (CatName, CommissinRate) Values('$CatName, $CommissionRate");
-        if($query){
-            echo"<script>alert(data inserted successfully)</script>";
-
-        } else{
-            echo"<script>alert(there is an error)</script>";
-
-        }
-    }
 
 
 
