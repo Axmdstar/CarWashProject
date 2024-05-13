0bc5b07a7c4f74eabfd35a404829829888470317
Abdirahmantahlil <hamzeshurie70@gmail.com>
2024-05-13 10:19:57 +0300

    added users

---
+++ User/User.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <?php include "../Components/HeadContent.php" ?>
+  
+  <body>
+    
+  <?php include '../Components/NavBar.php' ?>
+
+  
+  <main id="main" class="main">
+        <!-- Content Here  -->
+        <p>hello Class c</p>
+  </main>
+      
+    
+  </body>
+</html>
